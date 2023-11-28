45b801ec4452e0cf12f97e79772867a9c4f774b2
sahilagrawal15 <54533751+sahilagrawal15@users.noreply.github.com>
2023-11-28 11:13:21 -0500

    Added group 2 code for KNN, Naive bais, random forest and rpart

---
+++ group2/lab3_ctree3.R
@@ -0,0 +1,8 @@
+library(partykit)
+
+fitK <- ctree(Kyphosis ~ Age + Number + Start, data=kyphosis)
+plot(fitK, main="Conditional Inference Tree for Kyphosis”)
+plot(fitK, main="Conditional Inference Tree for Kyphosis",type="simple")
+
+#etc.
+
