198a3e29ce236ed87a255c8d6d80f985dbd39097
sahilagrawal15 <54533751+sahilagrawal15@users.noreply.github.com>
2023-12-05 11:21:59 -0500

    Support Vector machine, loess regression, cross validation exercises

---
+++ group4/lab1_cv7.R
@@ -0,0 +1,7 @@
+library(cvTools)
+library(robustbase)
+set.seed(2143) # set seed for reproducibility
+cvFolds(20, K = 5, type = "random")
+cvFolds(20, K = 5, type = "consecutive")
+cvFolds(20, K = 5, type = "interleaved")
+cvFolds(20, K = 5, R = 10)
